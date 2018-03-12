e5a5c419e65c448dfc17a9e70d319758679e2d30
VivianMugure <vivianmugure@gmail.com>
2018-03-12 07:49:08 +0000

    Our game

---
+++ vivi.js
@@ -0,0 +1,12 @@
+function setup (){
+createCanvas(1000,800);
+background(255,150,255);
+ellipse (500,400,80,80);
+}
+  
+function draw
+(){createCanvas(1000,800);
+background(255,150,255);
+ellipse (500,400,80,80);
+fill(207,219,140);
+}
